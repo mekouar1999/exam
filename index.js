fd73f3eee49aa2e90faed8fe94eb18395d7eab9f
mekouar1999 <othman.mekouar99@gmail.com>
2025-05-19 16:25:42 +0100

    fsdfsdfsdf

---
+++ index.js
@@ -0,0 +1,8 @@
+const toggle = document.getElementById("toggle");
+console.log(toggle);
+
+const navbar = document.getElementById('navbar');
+
+toggle.addEventListener('click',()=>{
+    navbar.classList.add('othman')
+})
